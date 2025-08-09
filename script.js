a014e4a104462d44546b131fbcc9b03a5de20d75
prakash97190 <theprakash97190@gmail.com>
2025-08-09 13:57:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Message Sent! (This is just a demo, no backend yet.)");
+});
